ff040df625650105d155bf9f2109d0ec92f58bf4
Sylvain Boily <sboily@proformatique.com>
2016-01-20 14:13:11 -0500

    refactor code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,42 @@
-auth = new XiVOAuth();
+/*
+@licstart  The following is the entire license notice for the 
+JavaScript code in this page.
 
-var set_backends = function (backends) {
+Copyright (C) 2015  Sylvain Boily
+
+The JavaScript code in this page is free software: you can
+redistribute it and/or modify it under the terms of the GNU
+General Public License (GNU GPL) as published by the Free Software
+Foundation, either version 3 of the License, or (at your option)
+any later version.  The code is distributed WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+As additional permission under GNU GPL version 3 section 7, you
+may distribute non-source (e.g., minimized or compacted) forms of
+that code without the copy of the GNU GPL normally required by
+section 4, provided you include this license notice and a URL
+through which recipients can access the Corresponding Source.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this page.
+*/
+
+auth = new XiVOAuth();
+var session = Cookies.getJSON('xivo_auth_session');
+var background_login = ["img/1.jpg"];
+
+var set_backends = function () {
     $('#backend').find('option').remove();
-    if (backends) {
-        $.each(backends['data'], function(index, element) {
+    $('#error').html('').addClass('hidden');
+
+    auth.host = $("input#host").val();
+    auth.backend().done(print_backends).fail(auth_error);
+}
+
+var print_backends = function(data) {
+    if (data) {
+        $.each(data['data'], function(index, element) {
             text = 'With ' + element.toUpperCase().replace('_',' ');
             value = element;
             var option = new Option(text, value);
@@ -14,18 +47,19 @@ var set_backends = function (backends) {
     $(".loader").fadeOut("slow");
 }
 
-var set_cookies = function(session) {
-    Cookies.set('xivo_auth_session', session['data']['token']);
-    Cookies.set('xivo_auth_uuid', session['data']['xivo_user_uuid']);
-    Cookies.set('xivo_auth_acls', session['data']['acls']);
-    Cookies.set('xivo_auth_auth_id', session['data']['auth_id']);
+var set_cookies = function(data) {
+    session = { token: data['data']['token'],
+                uuid: data['data']['xivo_user_uuid'],
+                acls: data['data']['acls'],
+                auth_id: data['data']['auth_id'],
+                expires: new Date(data['data']['expires_at']),
+                auth_host: auth.host
+              };
+    Cookies.set('xivo_auth_session', session, { expires: session.expires });
 }
 
-var unset_cookies = function(data) {
+var unset_cookies = function() {
     Cookies.remove('xivo_auth_session');
-    Cookies.remove('xivo_auth_uuid');
-    Cookies.remove('xivo_auth_acls');
-    Cookies.remove('xivo_auth_auth_id');
     $(".loader").fadeOut("slow");
     location.reload();
 }
@@ -33,64 +67,61 @@ var unset_cookies = function(data) {
 var logout = function() {
     $('#logout').on('click', function(e) {
         e.preventDefault();
-        auth.logout(Cookies.get('xivo_auth_session')).done(unset_cookies);
+        auth.host = session.auth_host;
+        auth.logout(session.token).done(unset_cookies);
     });
 }
 
-var launch_application = function(session) {
-    $('#login').hide();
-    $('#main').show();
-    $('#error').addClass('hide');
+var launch_application = function(data) {
     $.backstretch("destroy");
-    if (session)
-        set_cookies(session);
-    info = "<tr><td><strong>token</strong></td><td>" + Cookies.get('xivo_auth_session') + "</td></tr>" +
-           "<tr><td><strong>uuid</strong></td><td>" + Cookies.get('xivo_auth_uuid') + "</td></tr>" +
-           "<tr><td><strong>auth_id</strong></td><td>" + Cookies.get('xivo_auth_auth_id') + "</td></tr>" +
-           "<tr><td><strong>acls</strong></td><td>" + Cookies.get('xivo_auth_acls') + "</td></tr>";
-    $('#auth').html(info);
+
+    $('#error').addClass('hidden');
+    $('#login').addClass('hidden');
+    $('#main').removeClass('hidden');
+
+    if (data) { set_cookies(data); }
+
+    print_auth_info();
     logout();
 }
 
-var set_host = function() {
-    if (Cookies.get('xivo_server')) {
-        $('#host').val(Cookies.get('xivo_server'));
-    }
+var print_auth_info = function() {
+    info = "<tr><td><strong>token</strong></td><td>" + session.token + "</td></tr>" +
+           "<tr><td><strong>uuid</strong></td><td>" + session.uuid + "</td></tr>" +
+           "<tr><td><strong>auth_id</strong></td><td>" + session.auth_id + "</td></tr>" +
+           "<tr><td><strong>acls</strong></td><td>" + session.acls + "</td></tr>";
 
-    $('#host').change(function() {
-        Cookies.set('xivo_server', $('#host').val());
-        location.reload();
-    });
-
-    auth.host = $("input#host").val();
-
-    if (!auth.host) {
-        $('#tutorial').modal();
-    }
+    $('#auth').html(info);
 }
 
 var auth_error = function(data) {
-    console.log(data.status);
-    console.log(data.statusText);
-    message = "<p class='text-left'><strong>Error:</strong> "+ data.status +"<br><strong>Message:</strong> "+ data.statusText + "</p>";
+    $('#error').removeClass('hidden');
+
+    $('<p>', {
+        'class': 'text-left',
+        'html': '<strong>Error:</strong> '+ data.status +'<br><strong>Message:</strong>' + data.statusText,
+    }).appendTo('#error');
+
     if (data.status == 0) {
-        server = Cookies.get('xivo_server') + "/0.1/backends";
-        message += "<p class='text-left'><strong>Important:</strong> Please click on this <strong><a href='" +
-                    server + "' target='_blank'>link</a></strong> to check your service!</p>";
+        $('<p>', {
+            'class': 'text-left',
+            'html': '<strong>Important:</strong> Please click on this <strong><a href="' +
+                    auth.host + '" target="_blank">link</a></strong> to check your service!</p>'
+        }).appendTo('#error');
     }
-    $('#error').removeClass('hide');
-    $('#error').html(message);
+
     $(".loader").fadeOut("slow");
 }
 
 var launch_login = function() {
-    $('#main').hide();
-    $.backstretch(["img/1.jpg"], {centeredY: false});
+    $('#login').removeClass('hidden');
+    $.backstretch(background_login, {centeredY: false});
     $('[data-toggle="tooltip"]').tooltip()
 
     if ($("input#host").val()) {
-        auth.backend().done(set_backends).fail(auth_error);
+        set_backends();
     } else {
+        $('#tutorial').modal();
         $(".loader").fadeOut("slow");
     }
 
@@ -103,12 +134,13 @@ var launch_login = function() {
         auth.login(c).done(launch_application).fail(auth_error);
     });
 
+    $('#host').change(function() {
+        set_backends();
+    });
 }
 
 $(document).ready(function() {
-    set_host();
-
-    if (Cookies.get('xivo_auth_session') == null) {
+    if (session == null) {
         launch_login();
     } else {
         launch_application();
